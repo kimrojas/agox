7622dc26e84197e81722dbd62a77d4cfc1914e46
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2022-03-11 11:35:07 +0100

    Removed reference to LGPR.

--- agox/modules/models/__init__.py
+++ agox/modules/models/__init__.py
@@ -1,5 +1,1 @@
-from .sparse_models.model_LSGPR import LSGPRModel
-from .sparse_models.model_LSGPR_cluster import LSGPRModelCluster
-from .sparse_models.model_LSGPR_MBKMeans import LSGPRModelMBKMeans
-from .sparse_models.model_LSGPR_FPS import LSGPRModelFPS
 from .model_GPR import ModelGPR
