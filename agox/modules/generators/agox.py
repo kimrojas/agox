61a7baeaaddc60ff1e5082e19ba6d00bd5ec940a
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2022-06-29 10:24:50 +0200

    Updated AGOX Generator.

--- agox/modules/generators/agox.py
+++ agox/modules/generators/agox.py
@@ -1,25 +1,35 @@
 import numpy as np
 from agox.modules.generators.ABC_generator import GeneratorBaseClass
 from agox.modules.databases import Database
+from agox.main import AGOX
 
 class AGOXGenerator(GeneratorBaseClass):
 
     name = 'AGOX generator'
 
-    def __init__(self, agox, database, model, iterations=50, **kwargs):
+    def __init__(self, params=[], database=None, main_database=None, iterations=50, **kwargs):
         super().__init__(**kwargs)
-        self.agox = agox
+        self.params = params
         self.iterations = iterations
         self.database = database
-        self.model = model
+        self.main_database = main_database
 
     def get_candidates(self, sample, environment):
+        self.database.reset()
+        # Add the data from the main database, such that it can be used for sampling/training. 
+        [self.database.store_candidate(candidate, dispatch=False) for candidate in self.main_database.get_all_candidates()]
+
+        print('##### LENGTH OF DATABASE BEFORE: {}'.format(len(self.database)))
+
+        self.agox = AGOX(*self.params)
         self.agox.iteration_counter = 0
         self.agox.iteration_cache = {}
-        candidate = sample.get_random_member()
-        candidate.set_calculator(self.model)
-        self.database.candidates = [candidate]
-        self.database.candidate_energies = [candidate.get_potential_energy()]
+        self.database.candidate_energies = []
         self.agox.run(N_iterations=self.iterations, verbose=True)
-        return self.database.candidates[1:]
+
+        # Return only the candidates that we have produced
+        print('##### LENGTH OF DATABASE AFTER: {}'.format(len(self.database)))
+        print('##### LENGTH OF MAIN AFTER: {}'.format(len(self.main_database)))
+
+        return self.database.get_all_candidates()[len(self.main_database):]
 
