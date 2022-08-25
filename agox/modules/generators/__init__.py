3ba8ee86bbf411d5984bedfa2b968b224bd4f58e
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2022-08-25 09:22:15 +0200

    Added new generator Sampling.

--- agox/modules/generators/__init__.py
+++ agox/modules/generators/__init__.py
@@ -5,4 +5,4 @@ from agox.modules.generators.reuse import ReuseGenerator
 from agox.modules.generators.permutation import PermutationGenerator
 from agox.modules.generators.steepest_descent import SteepestDescentGenerator
 from agox.modules.generators.cog import CenterOfGeometryGenerator
-
+from agox.modules.generators.sampling import SamplingGenerator
