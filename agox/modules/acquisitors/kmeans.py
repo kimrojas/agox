0b405f18cdb84d34a39a89c26db09c16c82452a5
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2022-09-05 11:34:34 +0200

    Added energy threshold, changed output sorting and improved usability with other features.

--- agox/modules/acquisitors/kmeans.py
+++ agox/modules/acquisitors/kmeans.py
@@ -7,12 +7,14 @@ class KmeansAcquisitor(AcquisitorBaseClass):
 
     name = 'KmeansAcquisitor'
 
-    def __init__(self, descriptor, model, k=5, start_iteration=0, **kwargs):
+    def __init__(self, descriptor, model, k=5, energy_threshold=5, start_iteration=0, **kwargs):
         super().__init__(**kwargs)
         self.descriptor = descriptor
         self.model = model
         self.k = k
         self.start_iteration = start_iteration
+
+        self.energy_threshold = energy_threshold
 
     def calculate_acquisition_function(self, candidates):    
         pass
@@ -21,15 +23,27 @@ class KmeansAcquisitor(AcquisitorBaseClass):
         # Calculate features for all candidates:
 
         if self.get_iteration_counter() >= self.start_iteration and self.model.ready_state: 
+
+            # Filter according to energy:
+            [candidate.set_calculator(self.model) for candidate in candidates]
+            energies = np.array([a.get_potential_energy() for a in candidates])
+            valid_data = np.argwhere(energies <= np.min(energies) + self.energy_threshold).flatten()
+            candidates = [candidates[i] for i in valid_data]
+
             X = np.array([self.descriptor.get_feature(candidate) for candidate in candidates])
-            X = np.sum(X, axis=1)
+            if X.ndim == 3: # (Structure, atoms, features) -- sum away the atoms dimension to get (structure, features)                
+                X = np.sum(X, axis=1)
             k_means = KMeans(init="k-means++", n_clusters=self.k, n_init=10)
             labels = k_means.fit_predict(X)
             centers = k_means.cluster_centers_
             selected = self.select_from_clustering(candidates, labels, centers)
 
             selected_candidates = [candidates[i] for i in selected]
+            selected_energies = [atoms.get_potential_energy() for atoms in selected_candidates]
             acquisition_values = [labels[i] for i in selected]
+
+            sort_index = np.argsort(selected_energies)
+            selected_candidates = [selected_candidates[i] for i in sort_index]
 
             for cand, acq_value in zip(selected_candidates, acquisition_values):
                 self.writer(f'Model energy = {cand.get_potential_energy()} - Cluster label = {acq_value}')
@@ -41,7 +55,6 @@ class KmeansAcquisitor(AcquisitorBaseClass):
 
     def select_from_clustering(self, candidates, labels, cluster_centers):        
         # Select lowest energy from each cluster: 
-        [candidate.set_calculator(self.model) for candidate in candidates]
         energies = np.array([candidate.get_potential_energy() for candidate in candidates])
 
         unique_labels = np.unique(labels)
