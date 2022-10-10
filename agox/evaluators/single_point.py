821b1db6b64bd2955b38e81535089844878fdd95
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2022-10-10 11:32:56 +0200

    Fixed the calculator not accepting key word arguments.

--- agox/evaluators/single_point.py
+++ agox/evaluators/single_point.py
@@ -1,11 +1,8 @@
-from .ABC_evaluator import EvaluatorBaseClass
 from agox.evaluators.local_optimization import LocalOptimizationEvaluator
-import numpy as np
-from ase.calculators.singlepoint import SinglePointCalculator
 
 class SinglePointEvaluator(LocalOptimizationEvaluator):
 
     name = 'SinglePointEvaluator'
 
-    def __init__(self, calculator): 
-        super().__init__(calculator, optimizer_run_kwargs=dict(steps=0))
+    def __init__(self, calculator, **kwargs): 
+        super().__init__(calculator, optimizer_run_kwargs=dict(steps=0), **kwargs)
