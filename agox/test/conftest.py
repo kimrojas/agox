2754c7c0923579b54e266341c3ce06ec485cdc85
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2023-02-06 16:35:13 +0100

    Added cmd options.

--- agox/test/conftest.py
+++ agox/test/conftest.py
@@ -1,29 +1,11 @@
 import pytest
 
-strict_tolerances = {
-                    'energy':1E-9, 
-                    'positions':1E-9
-                    }
-
-medium_tolerances = {
-                    'energy':1E-6,
-                    'positions':1E-6
-                    }
-
-lose_tolerances =   {
-                    'energy':1E-3,
-                    'positions':1E-3,
-                    }
-
-tolerance_levels = {
-    'lose':lose_tolerances,
-    'medium':medium_tolerances,
-    'strict':strict_tolerances,
-}
-
 def pytest_addoption(parser):
-    parser.addoption('--tolerance', type=str, default='lose')
+    parser.addoption('--rtol', type=float, default=1e-05)
+    parser.addoption('--atol', type=float, default=1e-08)
+    parser.addoption('--create_mode', default=False, action='store_true')
 
 @pytest.fixture
 def cmd_options(request):
-    return {'tolerance':tolerance_levels[request.config.getoption('tolerance')]}
+    return {'tolerance':{'rtol':request.config.getoption('rtol'), 'atol':request.config.getoption('atol')},
+            'create_mode':request.config.getoption('create_mode')}
