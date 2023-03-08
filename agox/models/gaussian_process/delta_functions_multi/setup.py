f1dad6dc7202fe31e4100899acf6421f191fb277
Nikolaj Rønne <nronne@phys.au.dk>
2023-03-08 15:50:39 +0100

    removed old models

--- agox/models/gaussian_process/delta_functions_multi/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(ext_modules=cythonize('delta.pyx'), include_dirs=[numpy.get_include()])
--- agox/models/gaussian_process/featureCalculators_multi/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(ext_modules=cythonize('angular_fingerprintFeature_cy.pyx'), include_dirs=[numpy.get_include()])
