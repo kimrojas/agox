48e54142e6ea09c56886a40793d0e5e17601df2b
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2023-05-08 17:25:40 +0200

    Added a config parser and a default config file.

---
+++ agox/utils/config.py
@@ -0,0 +1,25 @@
+import configparser
+import inspect
+import os
+from pathlib import Path
+
+class Config:
+
+    def __init__(self, config_path):
+        self.parser = configparser.ConfigParser()
+        self.parser.read(config_path)
+
+def get_default_config_path():
+    agox_path = os.path.abspath(__file__) #inspect.getfile(AGOX)
+    agox_path = Path(os.path.dirname(os.path.dirname(os.path.dirname(agox_path))))
+    return agox_path / 'default_config.ini'
+
+config_path = os.environ.get('AGOX_CONFIG_PATH')
+if config_path is None:
+    config_path = get_default_config_path()
+
+print('Using config file: {}'.format(config_path))
+
+cfg = Config(config_path)
+
+
