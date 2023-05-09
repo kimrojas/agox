cd86c68072d473ab24b95f9af5abe1277c9ae645
Mads-Peter Verner Christiansen <machri@phys.au.dk>
2023-05-09 13:19:19 +0200

    Added module descriptions in __init__.py files.

--- agox/generators/__init__.py
+++ agox/generators/__init__.py
@@ -1,8 +1,8 @@
 """
 Generators are are responsible for generating new configurations. 
 Typically there are two types of generators: 
-    - Those that can generate a candidate by modifying one or more configurations. 
-    - Those that can generate a candidate without any input configurations.
+- Those that can generate a candidate by modifying one or more configurations. 
+- Those that can generate a candidate without any input configurations.
 
 The first catagory of generators require a Sampler to sample the configurations
 that it uses as a basis for generating a new candidate. The second category of
@@ -10,8 +10,8 @@ generators do not require a sampler.
 
 The generators have to generate a candidate that follows the rules set by the
 environment, that is
-    - The correct stoichoimetry
-    - Inside the confinement box. 
+- The correct stoichoimetry
+- Inside the confinement box. 
 """
 from agox.generators.random import RandomGenerator
 from agox.generators.rattle import RattleGenerator
